b470bb7715efab2fff21b9863fee7bda44187b0b
dengj <jie.dengjie@dianrong.com>
2016-12-01 14:40:41 +0800

    初始化提交代码

---
+++ src/testjava/TestJava.java
@@ -0,0 +1,12 @@
+package testjava;
+
+public class TestJava {
+
+	public static final int v =9;
+	public static void main(String[] args){
+		
+		if(v==9){
+			System.out.println("sdafasdfsaf");
+		}
+	}
+}
